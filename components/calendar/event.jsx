c1112b48dab9f48a43a6c76256305b9788a18540
Isaac Koh <isaackoh88@gmail.com>
2022-07-17 03:05:58 +0800

    Added. Show event description on click

--- components/calendar/event.jsx
+++ components/calendar/event.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Event = ({ event, categories }) => {
+    const [expand, setExpand] = useState(false);
+
     const getCategoryColor = Color => {
         if (Color === 'rose') {
             return 'bg-rose-600';
@@ -41,10 +43,35 @@ const Event = ({ event, categories }) => {
         };
     };
 
+    /** toggle event description display */
+    const handleClickDetails = () => {
+        setExpand(!expand);
+    };
+
     return (
-        <div className={`group cursor-pointer flex flex-row justify-between items-center w-full px-3 py-2 my-2 rounded-lg ${getCategoryColor(categories.find(e => e['Name'] === event['Category'])['Color'])}`}>
-            <p className='text-slate-200 group-hover:text-white duration-150 ease-in-out'>{ event['Name'] }</p>
-            <p className='text-slate-200 group-hover:text-white duration-150 ease-in-out'>{ event['StartTime'] } - { event['EndTime'] }</p>
+        <div 
+            className={`group cursor-pointer flex flex-col justify-center items-center w-full px-3 py-2 my-2 rounded-lg duration-150 ${getCategoryColor(categories.find(e => e['Name'] === event['Category'])['Color'])}`}
+            onClick={() => handleClickDetails()}
+        >
+            <div className='flex flex-row justify-between items-center w-full'>
+                <p className={`font-medium duration-150 ease-in-out ${expand ? 'text-white text-xl' : 'text-slate-200 group-hover:text-white'}`}>{ event['Name'] }</p>
+                <p className={`font-medium duration-150 ease-in-out ${expand ? 'text-white text-xl' : 'text-slate-200 group-hover:text-white'}`}>{ event['StartTime'] } - { event['EndTime'] }</p>
+            </div>
+            <p className={`text-white w-full mt-1 ${expand ? '' : 'hidden'}`}>{ event['Description'] }</p>
+            <div className={`flex flex-row justify-end items-center w-full mt-2 mb-1 ${expand ? '' : 'hidden'}`}>
+                <div 
+                    className='cursor-pointer bg-slate-200 px-3 py-2 mr-2 rounded-md'
+                    onClick={() => {/** Edit Event */}}
+                >
+                    <p className='text-blue-500 font-semibold duration-150 ease-in-out'>Edit Event</p>
+                </div>
+                <div 
+                    className='group cursor-pointer bg-slate-200 px-3 py-2 ml-2 rounded-md'
+                    onClick={() => {/** Delete Event */}}
+                >
+                    <p className='text-red-500 font-semibold duration-150 ease-in-out'>Delete Event</p>
+                </div>
+            </div>
         </div>
     );
 };
