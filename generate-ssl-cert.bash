5a3796332046b33a5791c82b040e436aff3a3270
Isaac Koh <isaackoh88@gmail.com>
2022-07-18 05:49:57 +0800

    Added. OpenSSL certificate generation.

---
+++ generate-ssl-cert.bash
@@ -0,0 +1,6 @@
+mkdir -p app/certs
+openssl req -x509 -out app/certs/localhost.crt -keyout app/certs/localhost.key \
+    -days 365 \
+    -newkey rsa:2048 -nodes -sha256 \
+    -subj '/CN=localhost' -extensions EXT -config <( \
+    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
