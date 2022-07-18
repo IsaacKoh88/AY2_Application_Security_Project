5153e17c6d9099f6be9069904091f6c8e2087935
Isaac Koh <isaackoh88@gmail.com>
2022-07-18 10:02:50 +0800

    Added. SSL cert

--- generate-ssl-cert.bash
+++ generate-ssl-cert.bash
@@ -4,3 +4,4 @@ openssl req -x509 -out app/certs/localhost.crt -keyout app/certs/localhost.key \
     -newkey rsa:2048 -nodes -sha256 \
     -subj '/CN=localhost' -extensions EXT -config <( \
     printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain app/certs/localhost.crt
