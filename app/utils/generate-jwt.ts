5a53a98cf6eec592b343e35e4d15e08dda689543
Isaac Koh <isaackoh88@gmail.com>
2022-07-20 17:42:28 +0800

    Added. Issuer in jwt.

--- app/utils/generate-jwt.ts
+++ app/utils/generate-jwt.ts
@@ -4,9 +4,9 @@ const generateJWT = async (email: string) => {
     return await new jose.SignJWT({ email: email })
     .setProtectedHeader({ alg: 'HS256' })
     .setIssuedAt()
+    .setIssuer('application-security-project')
     .setExpirationTime('30d')
-    .sign(new TextEncoder().encode(`qwertyuiop`))
-    .then(value => {return value});
+    .sign(new TextEncoder().encode(`qwertyuiop`));
 };
 
 export default generateJWT;
