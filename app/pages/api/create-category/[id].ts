78d115f7383ac41484e5afa95e0fa4dcdf2857de
Isaac Koh <isaackoh88@gmail.com>
2022-07-20 20:21:40 +0800

    Added. Validate authorisation module.

--- app/pages/api/create-category/[id].ts
+++ app/pages/api/create-category/[id].ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import executeQuery from '../../../utils/db'
+import authorisedValidator from '../../../utils/authorised-validator';
 import * as jose from 'jose'
 import { v4 as uuidv4 } from 'uuid';
 
@@ -15,44 +16,24 @@ export default async function CreateCategory(
 ) {
     /* accepts only POST requests and non-empty requests */
     if ((req.method == 'POST') && (req.body) && (req.cookies['token'])) {
-        const JWTtoken:string = req.cookies['token']!;
+        /** check user authorisation */
+        await authorisedValidator(req, res);
 
-        try {
-            /* check if JWT token is valid, then get the email */
-            const email = await jose.jwtVerify(JWTtoken, new TextEncoder()
-                        .encode(`qwertyuiop`))
-                        .then(value => {return value['payload']['email']});
-            
-            /* get uuid from email */
-            const resultID = await executeQuery({
-                query: 'select id from account where email = ?',
-                values: [email],
-            });
+        /** deconstruct body data */
+        const { categoryName, categoryColor } = req.body;
 
-            if (resultID[0].id === req.query.id) {
-                /** deconstruct body data */
-                const { categoryName, categoryColor } = req.body;
+        /** generate uuidv4 */
+        const id = uuidv4();
 
-                /** generate uuidv4 */
-                const id = uuidv4();
+        /* insert data into category table */
+        const result = await executeQuery({
+            query: 'INSERT INTO category VALUES(?, ?, ?, ?)',
+            values: [req.query.id, id, categoryName, categoryColor],
+        });
 
-                /* insert data into category table */
-                const result = await executeQuery({
-                    query: 'INSERT INTO category VALUES(?, ?, ?, ?)',
-                    values: [resultID[0].id, id, categoryName, categoryColor],
-                });
-
-                res.status(201).json({ ID: id, Name: categoryName, Color: categoryColor})
-                res.end();
-                return
-            }
-        } 
-        /* reject if JWT token is invalid */
-        catch (error) {
-            res.statusCode = 403;
-            res.end('Error');
-            return
-        }    
+        res.status(201).json({ ID: id, Name: categoryName, Color: categoryColor})
+        res.end();
+        return
     }
     /* rejects requests that are empty */
     else if (!req.body) {
--- app/pages/api/edit-category/[id].ts
+++ app/pages/api/edit-category/[id].ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import executeQuery from '../../../utils/db'
+import authorisedValidator from '../../../utils/authorised-validator';
 import * as jose from 'jose'
-import { v4 as uuidv4 } from 'uuid';
 
 type Data = {
     ID: string,
@@ -15,42 +15,21 @@ export default async function EditCategory(
 ) {
     /* accepts only POST requests and non-empty requests */
     if ((req.method == 'POST') && (req.body) && (req.cookies['token'])) {
-        const JWTtoken:string = req.cookies['token']!;
+        /** check user authorisation */
+        await authorisedValidator(req, res);
 
-        try {
-            /* check if JWT token is valid, then get the email */
-            const email = await jose.jwtVerify(JWTtoken, new TextEncoder()
-                        .encode(`qwertyuiop`))
-                        .then(value => {return value['payload']['email']});
-            
-            /* get uuid from email */
-            const resultID = await executeQuery({
-                query: 'select id from account where email = ?',
-                values: [email],
-            });
+        /** deconstruct body data */
+        const { categoryID, categoryName, categoryColor } = req.body;
 
-            if (resultID[0].id === req.query.id) {
-                /** deconstruct body data */
-                const { categoryID, categoryName, categoryColor } = req.body;
+        /* insert data into category table */
+        const result = await executeQuery({
+            query: 'UPDATE category SET Name=?, Color=? WHERE AccountID=? AND ID=?',
+            values: [categoryName, categoryColor, req.query.id, categoryID],
+        });
 
-                /* insert data into category table */
-                const result = await executeQuery({
-                    query: 'UPDATE category SET Name=?, Color=? WHERE AccountID=? AND ID=?',
-                    values: [categoryName, categoryColor, resultID[0].id, categoryID],
-                });
-                console.log(result)
-
-                res.status(201).json({ ID: categoryID, Name: categoryName, Color: categoryColor})
-                res.end();
-                return
-            }
-        } 
-        /* reject if JWT token is invalid */
-        catch (error) {
-            res.statusCode = 403;
-            res.end('Error');
-            return
-        }    
+        res.status(201).json({ ID: categoryID, Name: categoryName, Color: categoryColor})
+        res.end();
+        return
     }
     /* rejects requests that are empty */
     else if (!req.body) {
