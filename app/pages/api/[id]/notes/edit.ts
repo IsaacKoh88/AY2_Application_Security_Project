90a477ea4a42a3c8b25338224f304f3626c47334
Leroy <furnomaster164@gmail.com>
2022-07-23 03:40:18 +0800

    Added. Edit Notes API.

---
+++ app/pages/api/[id]/notes/edit.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import executeQuery from '../../../../utils/db'
+import authorisedValidator from '../../../../utils/authorised-validator';
+
+type Data = {
+    message: string
+}
+
+export default async function EditTodo(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    /* accepts only POST requests and non-empty requests */
+    if ((req.method == 'POST') && (req.body) && (req.cookies['token'])) {
+        /** check user authorisation */
+        await authorisedValidator(req, res);
+
+        /** deconstruct body data */
+        const { notesID, notesName, description } = req.body;
+
+        /* insert data into notes table */
+        const result = await executeQuery({
+            query: 'UPDATE todo SET Name=?, Date=? WHERE AccountID=? AND ID=?',
+            values: [notesName, description, req.query.id, notesID],
+        });
+
+        res.status(201).json({ message: 'success' })
+        res.end();
+        return
+    }
+    /* rejects requests that are empty */
+    else if (!req.body) {
+        res.statusCode = 405;
+        res.end('Error');
+        return
+    }
+}
