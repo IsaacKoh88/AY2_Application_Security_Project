bbe073d20f4155a9247cced441b542957f511808
Hd261004 <95203325+Hd261004@users.noreply.github.com>
2022-07-23 15:30:42 +0800

    Added. Budget Table

--- appsecproj.session.sql
+++ appsecproj.session.sql
@@ -1,5 +1,5 @@
 -- @block
-DROP TABLE IF EXISTS account, calendar, category, todo;
+DROP TABLE IF EXISTS account, events, category, todo, budget;
 
 CREATE TABLE account(
     id  VARCHAR(36) PRIMARY KEY NOT NULL UNIQUE,
@@ -26,11 +26,18 @@ CREATE TABLE category(
 );
 
 CREATE TABLE todo(
-    AccountID    VARCHAR(36) NOT NULL,
-    ID     VARCHAR(36) NOT NULL,
+    AccountID   VARCHAR(36) NOT NULL,
+    ID          VARCHAR(36) NOT NULL,
     Name        VARCHAR(255) NOT NULL,
     Date        DATE NOT NULL,
     Checked     TINYINT(4) NOT NULL
+);
+
+CREATE TABLE budget(
+    AccountID   VARCHAR(36) NOT NULL,
+    ID          VARCHAR(36) NOT NULL,
+    Name        VARCHAR(255) NOT NULL,
+    Amount      INT NOT NULL
 );
 
 
@@ -75,76 +82,76 @@ BEGIN
     SET @email = email;
     SET @password = password;
 
-    SET @id = uuid_v4s();
+    SET @ID = uuid_v4s();
 
     PREPARE stmt FROM 'SELECT count(*) FROM account where id=? INTO @count'; 
-    EXECUTE stmt USING @id;
+    EXECUTE stmt USING @ID;
     DEALLOCATE PREPARE stmt;
     
     WHILE (@count = 1)
     DO
-        SET @id = uuid_v4s();
+        SET @ID = uuid_v4s();
         PREPARE stmt FROM 'SELECT count(*) FROM account where id=? INTO @count'; 
-        EXECUTE stmt USING @id;
+        EXECUTE stmt USING @ID;
         DEALLOCATE PREPARE stmt;
     END WHILE;
     
     PREPARE stmt FROM 'INSERT INTO account VALUES (?, ?, ?)';
-    EXECUTE stmt USING @id, @email, @password;
+    EXECUTE stmt USING @ID, @email, @password;
     DEALLOCATE PREPARE stmt;
 END;
 
-CREATE PROCEDURE insertCalendarData (IN acct_id VARCHAR(36), date DATE, startTime TIME, endTime TIME, eventName VARCHAR(255), description VARCHAR(255), category_id VARCHAR(36))
+CREATE PROCEDURE insertCalendarData (IN AccountID VARCHAR(36), date DATE, startTime TIME, endTime TIME, eventName VARCHAR(255), description VARCHAR(255), category_id VARCHAR(36))
 BEGIN
-    SET @acct_id = acct_id;
-    SET @date = date;
-    SET @startTime = startTIme;
-    SET @endTime = endTime;
-    SET @eventName = eventName;
-    SET @description = description;
-    set @category_id = category_id;
+    SET @AccountID= acct_id;
+    SET @Date = date;
+    SET @StartTime = startTIme;
+    SET @EndTime = endTime;
+    SET @Name = eventName;
+    SET @Description = description;
+    set @CategoryID = category_id;
 
-    SET @event_id = uuid_v4s();
+    SET @ID = uuid_v4s();
 
-    PREPARE stmt FROM 'SELECT count(*) FROM calendar where acct_id = ? and event_id = ? INTO @count'; 
-    EXECUTE stmt USING @acct_id, @event_id;
+    PREPARE stmt FROM 'SELECT count(*) FROM events where AccountID = ? and ID = ? INTO @count'; 
+    EXECUTE stmt USING @AccountID, @ID;
     DEALLOCATE PREPARE stmt;
     
     WHILE (@count = 1)
     DO
-        SET @event_id = uuid_v4s();
-        PREPARE stmt FROM 'SELECT count(*) FROM calendar where acct_id = ? and event_id = ? INTO @count'; 
-        EXECUTE stmt USING @acct_id, @event_id;
+        SET @ID = uuid_v4s();
+        PREPARE stmt FROM 'SELECT count(*) FROM events where AccountID = ? and ID = ? INTO @count'; 
+        EXECUTE stmt USING @Accout, @ID;
         DEALLOCATE PREPARE stmt;
     END WHILE;
 
-    PREPARE stmt FROM 'INSERT INTO calendar VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-    EXECUTE stmt USING @acct_id, @event_id, @date, @startTime, @endTime, @eventName, @description, @category_id;
+    PREPARE stmt FROM 'INSERT INTO events VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    EXECUTE stmt USING @AccountID, @ID, @Date, @StartTime, @EndTime, @Name, @Description, @CategoryID;
     DEALLOCATE PREPARE stmt;
 END;
 
-CREATE PROCEDURE insertCategoryData (IN acct_id VARCHAR(36), categoryName VARCHAR(255), categoryColor VARCHAR(7))
+CREATE PROCEDURE insertCategoryData (IN AccountID VARCHAR(36), categoryName VARCHAR(255), categoryColor VARCHAR(8))
 BEGIN
-    SET @acct_id = acct_id;
-    SET @categoryName = categoryName;
-    SET @categoryColor = categoryColor;
+    SET @AccountID = AccountID;
+    SET @Name = categoryName;
+    SET @Color = categoryColor;
 
-    SET @category_id = uuid_v4s();
+    SET @ID = uuid_v4s();
 
-    PREPARE stmt FROM 'SELECT count(*) FROM category where acct_id = ? and category_id = ? INTO @count'; 
-    EXECUTE stmt USING @acct_id, @category_id;
+    PREPARE stmt FROM 'SELECT count(*) FROM category where AccountID = ? and ID = ? INTO @count'; 
+    EXECUTE stmt USING @AccountID, @ID;
     DEALLOCATE PREPARE stmt;
     
     WHILE (@count = 1)
     DO
-        SET @category_id = uuid_v4s();
-        PREPARE stmt FROM 'SELECT count(*) FROM category where acct_id = ? and category_id = ? INTO @count'; 
-        EXECUTE stmt USING @acct_id, @category_id;
+        SET @ID = uuid_v4s();
+        PREPARE stmt FROM 'SELECT count(*) FROM category where AccountID = ? and ID = ? INTO @count'; 
+        EXECUTE stmt USING @AccountID, @ID;
         DEALLOCATE PREPARE stmt;
     END WHILE;
 
     PREPARE stmt FROM 'INSERT INTO category VALUES (?, ?, ?, ?)';
-    EXECUTE stmt USING @acct_id, @category_id, @categoryName, @categoryColor;
+    EXECUTE stmt USING @AccountID, @ID, @Name, @Color;
     DEALLOCATE PREPARE stmt;
 END;
 
@@ -171,8 +178,33 @@ BEGIN
     PREPARE stmt FROM 'INSERT INTO todo VALUES (?, ?, ?, ?)';
     EXECUTE stmt USING @acct_id, @todo_id, @todoName, @todoDate;
     DEALLOCATE PREPARE stmt;
-END
+END;
 
+
+CREATE PROCEDURE insertBudgetData (IN AccountID VARCHAR(36), Name VARCHAR(255), Amount INT)
+BEGIN
+    SET @AccountID = AccountID;
+    SET @Name = Name;
+    SET @Amount = Amount;
+
+    SET @ID = uuid_v4s();
+
+    PREPARE stmt FROM 'SELECT count(*) FROM budget where AccountID = ? and ID = ? INTO @count'; 
+    EXECUTE stmt USING @AccountID, @ID;
+    DEALLOCATE PREPARE stmt;
+    
+    WHILE (@count = 1)
+    DO
+        SET @ID = uuid_v4s();
+        PREPARE stmt FROM 'SELECT count(*) FROM budget where AccountID = ? and ID = ? INTO @count'; 
+        EXECUTE stmt USING @AccountID, @ID;
+        DEALLOCATE PREPARE stmt;
+    END WHILE;
+
+    PREPARE stmt FROM 'INSERT INTO budget VALUES (?, ?, ?, ?)';
+    EXECUTE stmt USING @AccountID, @ID, @Name, @Amount;
+    DEALLOCATE PREPARE stmt;
+END;
 -- @block
 -- Consists of stored procedure that select data
 DROP PROCEDURE IF EXISTS selectEmail_Id;
@@ -196,14 +228,48 @@ BEGIN
     DEALLOCATE PREPARE stmt;
 END;
 
-
-
 -- @block
--- Got error, but it does insert data into the tables
-CALL insertAccountData('leroytan@mymail.com', '123456789');
-CALL insertAccountData('Hongda@mymail.com', '696912340');
-CALL insertAccountData('Isaackoh@yourmail.com', '0987654321');
-CALL insertAccountData('CChinshuan@ourmail.com', '1029384756')
+-- Consists of stored procedure that update data
+DROP PROCEDURE IF EXISTS updateAccount;
+DROP PROCEDURE IF EXISTS updateCategory;
+DROP PROCEDURE IF EXISTS updateEvent;
+DROP PROCEDURE IF EXISTS updateTodo;
+DROP PROCEDURE IF EXISTS updateExpense;
+
+CREATE PROCEDURE updateAccount (IN hashedPassword VARCHAR(255), id VARCHAR(36), email VARCHAR(255))
+BEGIN
+    SET @hashedPassword = hashedPassword;
+    SET @id = id;
+    SET @email = email;
+
+    PREPARE stmt FROM 'UPDATE account SET password=? WHERE id=? AND email=?';
+    EXECUTE stmt using @hashedPassword, @id, @email;
+    DEALLOCATE PREPARE stmt;
+END;
+
+CREATE PROCEDURE updateCategory (IN categoryName VARCHAR(255), categoryColor VARCHAR(7), account_id VARCHAR(36), category_id VARCHAR(36))
+BEGIN
+    SET @categoryName = categoryName;
+    SET @categoryColor = categoryColor;
+    SET @account_id = account_id;
+    SET @category_id = category_id;
+
+    PREPARE stmt FROM 'UPDATE category SET name=?, color=? WHERE account_id=? AND category_id=?';
+    EXECUTE stmt using @categoryName, @categoryColor, @account_id, @category_id;
+    DEALLOCATE PREPARE stmt;
+END;
+
+CREATE PROCEDURE updateEvent (IN categoryName VARCHAR(255), categoryColor VARCHAR(7), account_id VARCHAR(36), category_id VARCHAR(36))
+BEGIN
+    SET @categoryName = categoryName;
+    SET @categoryColor = categoryColor;
+    SET @account_id = account_id;
+    SET @category_id = category_id;
+
+    PREPARE stmt FROM 'UPDATE category SET name=?, color=? WHERE account_id=? AND category_id=?';
+    EXECUTE stmt using @categoryName, @categoryColor, @account_id, @category_id;
+    DEALLOCATE PREPARE stmt;
+END;
 
 -- @block
 select * from calendar;
@@ -216,3 +282,7 @@ select * from category;
 
 -- @block
 select * from todo;
+
+-- @block
+select * from budget;
+
