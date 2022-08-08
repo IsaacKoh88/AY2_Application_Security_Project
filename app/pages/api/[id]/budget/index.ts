214caef9202d2661f99c1a8f2c311d7600cd4083
Hd261004 <95203325+Hd261004@users.noreply.github.com>
2022-08-08 17:11:58 +0800

    Fixed. Edited circle style to implement CSP

--- app/pages/api/[id]/budget/index.ts
+++ app/pages/api/[id]/budget/index.ts
@@ -1,9 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import executeQuery from '../../../../utils/db'
 import authorisedValidator from '../../../../utils/authorised-validator';
+import dayjs from 'dayjs';
 
 type Data = {
     Budget: number,
+    circleStyle: string,
 }
 
 export default async function GetBudget(
@@ -15,16 +17,34 @@ export default async function GetBudget(
         /** check user authorisation */
         await authorisedValidator(req, res);
 
-        /** deconstruct request body */
-        const { Month } = req.body;
-
         /* get budget */
         const result = JSON.parse(JSON.stringify(await executeQuery({
             query: 'SELECT Budget FROM budget WHERE AccountID=?',
             values: [req.query.id],
         })));
 
-        res.status(200).json({ Budget: result[0].Budget })
+        const resultTotalExpense = JSON.parse(JSON.stringify(await executeQuery({
+            query: 'CALL selectSumExpense_AccountID_Month(?, ?)',
+            values: [req.query.id, dayjs().format('YYYY-MM-DD')],
+        })));
+
+        var totalExpense = 0
+        if (resultTotalExpense[0][0]['TotalExpense'] !== null) {
+            totalExpense = resultTotalExpense[0][0]['TotalExpense']
+        }
+
+        const expenseDifference = result[0].Budget - totalExpense
+        if (expenseDifference == 0){
+            var circleStyle = 'flex justify-center items-center bg-indigo-600 h-96 w-96 m-8 rounded-full'
+        }
+        else if (expenseDifference > 0) {
+            var circleStyle = 'flex justify-center items-center bg-lime-600 h-96 w-96 m-8 rounded-full'
+        }
+        else{
+            var circleStyle = 'flex justify-center items-center bg-red-700 h-96 w-96 m-8 rounded-full'
+        }
+
+        res.status(200).json({ Budget: result[0].Budget, circleStyle: circleStyle })
 
     };
 };
