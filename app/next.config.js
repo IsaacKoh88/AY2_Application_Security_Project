d63350a3c12b2cf0b070cb3cbef28edc5bb2999f
Hd261004 <95203325+Hd261004@users.noreply.github.com>
2022-08-14 19:11:04 +0800

    Fixed. CSP img src

--- app/next.config.js
+++ app/next.config.js
@@ -43,7 +43,9 @@ module.exports = () => {
 
                     {
                         "key": "Content-Security-Policy",
-                        "value": "default-src 'self'; script-src 'self' 'unsafe-eval'; img-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self';"
+                        "value": "default-src 'self'; script-src 'self' 'unsafe-eval'; img-src 'self'; font-src 'self';\
+                         style-src 'self' 'sha256-4/2nIlfwIVTJ1+JcNQ6LkeVWzNS148LKAJeL5yofdN4=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' 'sha256-17bm4kyfqQSAZqk3Q+0lFlkHTnSGpOoBSvtppRZq1AM=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' 'sha256-l5/ETC5uucCzv8WRWkDG6N1Br2Hnhib+jMX0smfAeS8=' 'sha256-x9xBo8vA8uMXU3yjSQdQY2obHev3Cdd9Sw9YotHDZGU=' 'sha256-MmexY5TvGETQJqrtJ6f8kSEYdd+y7gus2NY+YHH5/vM='\
+                         'sha256-Xhk1rMtpnLuhjAw8UNPGzuSTildNUS9rPgYeADF5xQM=' 'sha256-rOkfNP+Pcm0pog2/fsDp0vtd2DAdrSpsGpomAp3sIB8=';"
                     }
                 ]
             }
