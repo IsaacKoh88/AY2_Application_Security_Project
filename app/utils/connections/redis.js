9c7bac1a476190e926eeed5dccad2b650238c47f
Isaac Koh <isaackoh88@gmail.com>
2022-08-14 13:57:02 +0800

    Added. Temporary fix to docker compose build.

--- app/utils/connections/redis.js
+++ app/utils/connections/redis.js
@@ -1,8 +1,8 @@
 import { createClient } from 'redis';
 require('dotenv').config();
 
-const redisClient = createClient({
+const redisClient = createClient(/**{
     url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
-});
+}*/);
 
 export default redisClient;
